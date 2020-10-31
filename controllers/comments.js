0ac4cdc1f927ded5bd166a39c74e7f1316bd2c1f
Gabbytam <gtam2@fordham.edu>
2020-10-30 18:38:34 -0700

    changed some styling features, added a heading partial

--- controllers/comments.js
+++ controllers/comments.js
@@ -8,15 +8,46 @@ router.get('/', (req, res)=> {
 
 router.post('/', (req,res)=> {
     //console.log('article id', req.body.articleId);
-    db.comment.create({
-        name: req.body.name,
-        content: req.body.content,
-        articleId: req.body.articleId
+    // db.comment.create({
+    //     name: req.body.name,
+    //     content: req.body.content,
+    //     articleId: req.body.articleId
+    // })
+    // .then(createdComment=> {
+    //     console.log('YOUR NEW COMMENT', createdComment);
+    //     res.redirect(`/articles/${req.body.articleId}`);
+    // })
+
+    //above code works but this one is able to assign the comment an articleId by association, while the above is hard coded in
+    db.article.findOne({
+        where: {id: req.body.articleId}
     })
-    .then(createdComment=> {
-        console.log('YOUR NEW COMMENT', createdComment);
-        res.redirect(`/articles/${req.body.articleId}`);
+    .then(foundArticle=> {
+        foundArticle.createComment({
+            name: req.body.name,
+            content: req.body.content
+        })
+        .then(createdComment=> {
+            //console.log('your new comment', createdComment);
+            res.redirect(`/articles/${req.body.articleId}`);
+        })
     })
 })
 
 module.exports = router
+
+
+//IS THIS BETTER PRACTICE 
+// db.article.findOne({
+//     where: {id: req.body.articleId}
+// })
+// .then(foundArticle=> {
+//     foundArticle.createComment({
+//         name: req.body.name,
+//         content: req.body.content
+//     })
+//     .then(createdComment=> {
+//         console.log('your new comment', createdComment);
+//         res.redirect(`/articles/${req.body.articleId}`);
+//     })
+// })
